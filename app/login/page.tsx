6a040d9a64d7c4837acce4815d0cfc7ed3e42e47
GoLiveTechBrainon <brainon.queiroz@golivetech.com.br>
2025-01-16 22:34:01 -0300

    remove api dir

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/application/Header";
 import { MagicLinkLoginForm } from "@/components/login-form";
 
+// email para testar login: pecas.online.agora@gmail.com
+
 export default function Page() {
   return (
     <div className="flex flex-col h-screen w-full container mx-auto">
